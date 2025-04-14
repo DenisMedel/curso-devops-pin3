d490a126fef725ab4f679d68243d48e130efa58c
Denis <denis.medel@holon.com.ar>
2025-04-14 11:09:18 -0300

    Añade permisos al playbook para crear directorios

--- docker-compose/terraform/ec2-script.sh
+++ docker-compose/terraform/ec2-script.sh
@@ -2,6 +2,7 @@
 
 sudo apt update -y
 
+# Instala Docker
 sudo curl -fsSL https://get.docker.com | sh
 
 sudo usermod -aG docker ubuntu
@@ -9,6 +10,7 @@ sudo usermod -aG docker ubuntu
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# Instala Docker-Compose
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.34.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
